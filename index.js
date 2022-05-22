457c7f766a28e05b563aec771a55956f82274712
Md Rasel Shah <mdraselshah56@gmail.com>
2022-05-22 21:56:56 +0600

    server setup done

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qjnlv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const productCollection = client.db("orbitX-products").collection("products");
+async function run() {
+  try {
+    await client.connect();
+
+    app.get("/home", async (req, res) => {
+      const query = {};
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/purchase/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+  } finally {
+    //
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("orbitX connect");
+});
+
+app.listen(port, () => {
+  console.log("listen db", port);
+});
