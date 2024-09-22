cfdc9ff2364712524d6eb6363bed828ca65b40c4
RaselShah <mdraselshah56@gmail.com>
2024-09-22 12:31:25 +0600

    secret add

--- index.js
+++ index.js
@@ -186,6 +186,7 @@ async function run() {
 
     app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
+
       const user = await usersCollection.findOne({ email: email });
       const isAdmin = user.role === "admin";
       res.send({ admin: isAdmin });
@@ -223,7 +224,6 @@ async function run() {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
       const payment = req.body;
-      console.log(payment);
       const updateDoc = {
         $set: {
           paid: true,
